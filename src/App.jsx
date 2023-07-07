def07f11e7a64db9f6f6d80646fef0e96daec109
devchaeyoung <chaemaa00@gmail.com>
2023-07-07 17:35:13 +0900

    # 콘솔 수정

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,27 @@ function App() {
   useEffect(() => {
     getMovie();
   }, []); //디펜더싱 배열 (의존성 배열)
-  useEffect(() => {
-    console.log(movieList[0].runtime);
-  }, [movieList]);
+
   return (
     <div className="App">
       {movieList.length === 0 ? (
         "loading..."
       ) : (
         <>
-          <MovieItem cover={movieList[1].medium_cover_image} title={movieList[1].title} rating={movieList[1].rating} runtime={movieList[1].runtime} genres={movieList[1].genres} />
-          <MovieItem cover={movieList[0].medium_cover_image} title={movieList[0].title} rating={movieList[0].rating} runtime={movieList[0].runtime} genres={movieList[0].genres} />
+          <MovieItem
+            cover={movieList[1].medium_cover_image}
+            title={movieList[1].title}
+            rating={movieList[1].rating}
+            runtime={movieList[1].runtime}
+            genres={movieList[1].genres}
+          />
+          <MovieItem
+            cover={movieList[0].medium_cover_image}
+            title={movieList[0].title}
+            rating={movieList[0].rating}
+            runtime={movieList[0].runtime}
+            genres={movieList[0].genres}
+          />
         </>
       )}
     </div>
